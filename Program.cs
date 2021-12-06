432ef71b6136dcc1098d95d7dc6867c631c160d1
acallahan97 <callahanamanda877@gmail.com>
2021-12-06 13:33:25 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operators_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+         
+
+
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {remainder}");
+            }
+        }
+    }
+}
